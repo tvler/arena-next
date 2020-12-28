f326e65b264163ff177d351874f8293683c82eb5
Tyler Deitz <tylerdeitz@gmail.com>
2020-12-28 10:34:08 -0800

    create UserFollowing base components

---
+++ components/UserFollowingGrid.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from "@apollo/client";
+import { useCallback } from "react";
+import IntersectionObserverBox from "./IntersectionObserverBox";
+import { UserSsr, UserSsrVariables } from "../graphql/gen/UserSsr";
+import userSsr from "../graphql/queries/userSsr";
+import UserFollowingCard from "./UserFollowingCard";
+import { UserFollowingCardProps } from "./UserFollowingCard";
+
+const UserFollowingGrid: React.FC<{ id: string }> = ({ id }) => {
+  const serversideQuery = useQuery<UserSsr, UserSsrVariables>(userSsr, {
+    ssr: true,
+    variables: { id },
+  });
+  const user = serversideQuery.data?.identity?.identifiable;
+
+  const intersectionObserverCallback = useCallback<
+    (cellID: number) => (entries: IntersectionObserverEntry[]) => void
+  >(
+    (cellID) => (entries) => {
+      //
+    },
+    []
+  );
+
+  if (user?.__typename !== "User") {
+    return null;
+  }
+
+  const followingCount: number = user.counts?.following ?? 0;
+
+  return (
+    <div className="mt-6 pl-4 pr-4 grid grid-cols-auto-fit-block auto-rows-block gap-4">
+      {Array.from({ length: followingCount }, (_, i) => {
+        const followingId: null | number = null;
+
+        const userFollowingCardProps: UserFollowingCardProps = {
+          id: followingId ?? undefined,
+        };
+
+        return (
+          <IntersectionObserverBox
+            key={i}
+            Component={UserFollowingCard}
+            componentProps={userFollowingCardProps}
+            callback={intersectionObserverCallback}
+            id={i}
+            skip={followingId !== null}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default UserFollowingGrid;
