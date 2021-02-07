593d99170752377ff70a4a0027c186f07cc8462d
Tyler Deitz <tylerdeitz@gmail.com>
2021-02-07 15:01:35 -0800

    add initial mockfragment and testblock

---
+++ components/MockFragment.tsx
@@ -0,0 +1,23 @@
+import { useApolloClient, Cache } from "@apollo/client";
+import { ReactNode, useEffect } from "react";
+
+export const MockFragment: React.FC<{
+  children: JSX.Element;
+  fragment: Cache.WriteFragmentOptions<any, any>;
+}> = ({ children, fragment }) => {
+  const { cache } = useApolloClient();
+
+  useEffect(() => {
+    cache.writeFragment(fragment);
+
+    return () => {
+      const normalizedIdentity = cache.identify(fragment.data);
+      if (normalizedIdentity !== undefined) {
+        cache.evict({ id: normalizedIdentity });
+        cache.gc();
+      }
+    };
+  }, [cache, fragment]);
+
+  return children;
+};
--- components/TestBlocksPage.tsx
+++ components/TestBlocksPage.tsx
@@ -1,3 +1,27 @@
+import { Cache } from "@apollo/client";
+import { Block, BlockVariant } from "./Block";
+import { userCardFragmentNode } from "../graphql/fragments/UserCardFragment";
+import { UserCardFragment } from "../graphql/gen/UserCardFragment";
+import { MockFragment } from "./MockFragment";
+
 export const TestBlocksPage: React.FC = () => {
-  return null;
+  const userFragment: Cache.WriteFragmentOptions<UserCardFragment, null> = {
+    fragment: userCardFragmentNode,
+    data: {
+      __typename: "User",
+      id: 123,
+      name: "Tyler Deitz",
+      slug: "are-na",
+      initials: "TD",
+      avatar: null,
+    },
+  };
+
+  return (
+    <div className="grid grid-cols-auto-fit-block auto-rows-block gap-4 p-4">
+      <MockFragment fragment={userFragment}>
+        <Block id={123} variant={BlockVariant.user} />
+      </MockFragment>
+    </div>
+  );
 };
