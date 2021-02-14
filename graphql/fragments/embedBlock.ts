3f2e36c6a159f1267785edad27b2f0c08476e7b6
Tyler Deitz <tylerdeitz@gmail.com>
2021-02-14 13:53:09 -0800

    add embed fragment

---
+++ graphql/fragments/embedBlock.ts
@@ -0,0 +1,8 @@
+import { gql } from "@apollo/client";
+
+export const embedBlockFragmentNode = gql`
+  fragment EmbedBlockFragment on Embed {
+    id
+    image_url(size: DISPLAY)
+  }
+`;
---
+++ graphql/gen/EmbedBlockFragment.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: EmbedBlockFragment
+// ====================================================
+
+export interface EmbedBlockFragment {
+  __typename: "Embed";
+  id: number | null;
+  image_url: string | null;
+}
