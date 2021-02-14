3b56ab5e2813b8d09e7c7c287fe8cab5ef552e79
Tyler Deitz <tylerdeitz@gmail.com>
2021-02-14 11:08:55 -0800

    add ImageBlockFragment

---
+++ graphql/fragments/imageBlock.ts
@@ -0,0 +1,8 @@
+import { gql } from "@apollo/client";
+
+export const imageBlockFragmentNode = gql`
+  fragment ImageBlockFragment on Image {
+    id
+    image_url(size: DISPLAY)
+  }
+`;
---
+++ graphql/gen/ImageBlockFragment.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: ImageBlockFragment
+// ====================================================
+
+export interface ImageBlockFragment {
+  __typename: "Image";
+  id: number | null;
+  image_url: string | null;
+}
