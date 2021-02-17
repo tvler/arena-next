25a6baa4711e940120729eca8cb0b38c6ce85844
Tyler Deitz <tylerdeitz@gmail.com>
2021-02-17 09:21:56 -0800

    add channel pages to home screen

--- components/Home.tsx
+++ components/Home.tsx
@@ -7,17 +7,35 @@ export const Home: React.FC = () => {
       <Header taxonomy={["Explore"]} />
       <div className="flex flex-col pl-4 pr-4 items-start">
         <span className="text-xl">User pages</span>
+
         <Link href="/user/tyler-deitz">
           <a>/user/tyler-deitz</a>
         </Link>
+
         <Link href="/user/are-na">
           <a>/user/are-na</a>
         </Link>
 
+        <span className="text-xl mt-7">Channel pages</span>
+
+        <Link href="/channel/arena-next-journal">
+          <a>/channel/arena-next-journal</a>
+        </Link>
+
+        <Link href="channel/arena-influences">
+          <a>channel/arena-influences</a>
+        </Link>
+
+        <Link href="channel/image-board">
+          <a>channel/image-board</a>
+        </Link>
+
         <span className="text-xl mt-7">Test pages</span>
+
         <Link href="/test-block">
           <a>/test-block</a>
         </Link>
+
         <Link href="/test-blocks">
           <a>/test-blocks</a>
         </Link>
