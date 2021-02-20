c9375c5e7036cd65316242307d900da6b22f7822
Tyler Deitz <tylerdeitz@gmail.com>
2021-02-20 13:36:07 -0800

    solidify grid

--- components/grid3.tsx
+++ components/grid3.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useQuery } from "@apollo/client";
 import { DocumentNode } from "graphql";
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 import { ChannelBlockFragment } from "../graphql/gen/ChannelBlockFragment";
 import { GroupBlockFragment } from "../graphql/gen/GroupBlockFragment";
+import { TextBlockFragment } from "../graphql/gen/TextBlockFragment";
 import { UserBlockFragment } from "../graphql/gen/UserBlockFragment";
 import {
   UserFollowingQuery,
@@ -11,6 +12,12 @@ import {
   UserFollowingQuery_user,
 } from "../graphql/gen/UserFollowingQuery";
 import { userFollowingQueryNode } from "../graphql/queries/userFollowing";
+import { Block, BlockProps, BlockVariant } from "./Block";
+import { IntersectionObserverBox } from "./IntersectionObserverBox";
+
+/*
+ * Types
+ */
 
 type SubType<Base, Condition> = Pick<
   Base,
@@ -23,18 +30,21 @@ type NonNullableRecord<T> = {
   [key in keyof T]: NonNullable<T[key]>;
 };
 
-type BlockTypename = "User" | "Channel" | "Group";
-
-type A = NonNullableRecord<UserFollowingQuery_user>;
-
-type Test = keyof SubType<
-  NonNullableRecord<UserFollowingQuery_user>,
-  Array<{ __typename: BlockTypename } | null>
->;
-
-type BlockType = {
-  __typename: BlockTypename;
+type QueryVariables = {
+  id: string;
+  page: number;
+  per: number;
 };
+
+type BlockFragments =
+  | UserBlockFragment
+  | ChannelBlockFragment
+  | GroupBlockFragment
+  | TextBlockFragment;
+
+/*
+ * Utils
+ */
 
 const pageCount = 12;
 
@@ -42,25 +52,23 @@ const getPageNumberFromCellIndex = (cellIndex: number): number => {
   return Math.floor(cellIndex / pageCount) + 1;
 };
 
-type QueryVariables = {
-  id: string;
-  page: number;
-  per: number;
-};
+/*
+ * Component
+ */
 
 export function Grid<
   TData extends {
     [key in QueryField]:
       | null
       | {
-          [key in ContentField]: unknown;
+          [key in ContentField]: Array<BlockFragments | null> | null;
         };
   },
   TVariables extends QueryVariables,
   QueryField extends string | number | symbol = keyof TData,
   ContentField extends string | number | symbol = keyof SubType<
     NonNullableRecord<NonNullable<TData[QueryField]>>,
-    Array<UserBlockFragment | ChannelBlockFragment | GroupBlockFragment | null>
+    Array<BlockFragments | null>
   >
 >({
   contentCount,
@@ -86,17 +94,75 @@ export function Grid<
     },
   });
 
+  const intersectionObserverCallback = useCallback<
+    (cellID: number) => (entries: IntersectionObserverEntry[]) => void
+  >(
+    (cellID) => (entries) => {
+      for (const entry of entries) {
+        if (
+          entry.isIntersecting &&
+          fetchMore // sometimes fetchMore is undefined during hot reload
+        ) {
+          const page = getPageNumberFromCellIndex(cellID);
+          if (!queriedPagesRef.current.has(page)) {
+            queriedPagesRef.current.add(page);
+            fetchMore({
+              variables: {
+                page,
+              },
+            });
+          }
+        }
+      }
+    },
+    [fetchMore]
+  );
+
   const query = data && data[queryField];
   const content = query && query[contentField];
 
-  if (Array.isArray(content)) {
-    console.log(content);
-  }
-  // if (query) {
-  //   const field = query[contentField];
-  //   console.log();
-  // }
-  return null;
+  return (
+    <div className="pl-4 pr-4 grid grid-cols-auto-fit-block auto-rows-block gap-4">
+      {Array.from({ length: contentCount }, (_, i) => {
+        const followingItem = content && content[i];
+        let blockProps: BlockProps = {};
+
+        if (followingItem && followingItem.id !== null) {
+          switch (followingItem?.__typename) {
+            case "User":
+              blockProps = {
+                id: followingItem.id,
+                variant: BlockVariant.user,
+              };
+              break;
+            case "Channel":
+              blockProps = {
+                id: followingItem.id,
+                variant: BlockVariant.channel,
+              };
+              break;
+            case "Group":
+              blockProps = {
+                id: followingItem.id,
+                variant: BlockVariant.group,
+              };
+              break;
+          }
+        }
+
+        return (
+          <IntersectionObserverBox
+            key={i}
+            Component={Block}
+            componentProps={blockProps}
+            callback={intersectionObserverCallback}
+            id={i}
+            skip={followingItem !== null && followingItem !== undefined}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export const test = (
